220feb7e6323428917f6c87aa1a926d7d24b2967
Jarren <jarrenmcampos@gmail.com>
2020-08-20 20:07:34 -0700

    Added outlets to location swift file

--- ProjectManager/View Controllers/ReceiptsVC/AddLocationReceiptViewController.swift
+++ ProjectManager/View Controllers/ReceiptsVC/AddLocationReceiptViewController.swift
@@ -7,24 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
 class AddLocationReceiptViewController: UIViewController {
 
+    @IBOutlet var mapKitView: MKMapView!
+    @IBOutlet var locationTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func nextButtonPressed(_ sender: Any) {
+        
     }
-    */
-
+    
 }
