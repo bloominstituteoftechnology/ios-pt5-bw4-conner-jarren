43e90242f0a260fede8dcdd9abbeac81ca091f96
Jarren <jarrenmcampos@gmail.com>
2020-08-20 20:33:09 -0700

    Added outlets to detail from table view

--- ProjectManager/View Controllers/ReceiptsVC/DetailReceiptViewController.swift
+++ ProjectManager/View Controllers/ReceiptsVC/DetailReceiptViewController.swift
@@ -9,22 +9,19 @@
 import UIKit
 
 class DetailReceiptViewController: UIViewController {
-
+    @IBOutlet var receiptPreviewImageView: UIImageView!
+    
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var placeOfPurchaseLabel: UILabel!
+    @IBOutlet var itemNameLabel: UILabel!
+    @IBOutlet var priceLabel: UILabel!
+    @IBOutlet var categoryLabel: UILabel!
+    @IBOutlet var quantityLabel: UILabel!
+    @IBOutlet var descriptionTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
