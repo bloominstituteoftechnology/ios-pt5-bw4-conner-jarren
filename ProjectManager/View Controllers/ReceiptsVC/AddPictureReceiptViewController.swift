06af75cdd35e7de4ddd9cfb68120bb46dc0f131b
Jarren <jarrenmcampos@gmail.com>
2020-08-20 20:11:25 -0700

    Added outlets to picture swift file

--- ProjectManager/View Controllers/ReceiptsVC/AddPictureReceiptViewController.swift
+++ ProjectManager/View Controllers/ReceiptsVC/AddPictureReceiptViewController.swift
@@ -10,21 +10,18 @@ import UIKit
 
 class AddPictureReceiptViewController: UIViewController {
 
+    @IBOutlet var receiptPreviewImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addImagePressed(_ sender: Any) {
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func nextButtonPressed(_ sender: Any) {
+        
     }
-    */
-
 }
