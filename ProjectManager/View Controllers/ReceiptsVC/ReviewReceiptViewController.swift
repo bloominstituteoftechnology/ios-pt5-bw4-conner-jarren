779d9970516692a3b94b1df5ae308395254e8a9c
Jarren <jarrenmcampos@gmail.com>
2020-08-20 20:22:59 -0700

    added outlets to review receipt swift file

--- ProjectManager/View Controllers/ReceiptsVC/ReviewReceiptViewController.swift
+++ ProjectManager/View Controllers/ReceiptsVC/ReviewReceiptViewController.swift
@@ -9,22 +9,18 @@
 import UIKit
 
 class ReviewReceiptViewController: UIViewController {
-
+    @IBOutlet var receiptPreviewPicture: UIImageView!
+    
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var placeOfPurchaseLabel: UILabel!
+    @IBOutlet var itemNameLabel: UILabel!
+    @IBOutlet var priceLabel: UILabel!
+    @IBOutlet var categoryLabel: UILabel!
+    @IBOutlet var quantityLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
