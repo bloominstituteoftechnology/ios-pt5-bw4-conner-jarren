4cf19107b4a99713ea1a871f1b4de98e8811c5bb
Jarren <jarrenmcampos@gmail.com>
2020-08-20 19:43:29 -0700

    Added swift files for receipt flow

---
+++ ProjectManager/View Controllers/ReceiptsVC/AddLocationReceiptViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddLocationReceiptViewController.swift
+//  ProjectManager
+//
+//  Created by Jarren Campos on 8/20/20.
+//  Copyright © 2020 LambdaStudents. All rights reserved.
+//
+
+import UIKit
+
+class AddLocationReceiptViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
