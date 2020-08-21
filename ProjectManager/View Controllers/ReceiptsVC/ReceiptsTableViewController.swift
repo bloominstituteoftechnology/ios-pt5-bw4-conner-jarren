180c8c155f53212fa1d29d523dbb416079909cf6
Jarren <jarrenmcampos@gmail.com>
2020-08-20 18:57:35 -0700

    Added receipts table view controller file

---
+++ ProjectManager/View Controllers/ReceiptsVC/ReceiptsTableViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ReceiptsTableViewController.swift
+//  ProjectManager
+//
+//  Created by Jarren Campos on 8/20/20.
+//  Copyright © 2020 LambdaStudents. All rights reserved.
+//
+
+import UIKit
+
+class ReceiptsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "receiptCell", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+}
