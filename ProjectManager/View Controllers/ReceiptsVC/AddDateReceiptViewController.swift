d24d629b21f62d3dcd6995353554797f95c2ab4a
Jarren <jarrenmcampos@gmail.com>
2020-08-21 20:55:44 -0700

    Added flow through receipt creation through prepare for segues.

--- ProjectManager/View Controllers/ReceiptsVC/AddDateReceiptViewController.swift
+++ ProjectManager/View Controllers/ReceiptsVC/AddDateReceiptViewController.swift
@@ -10,15 +10,31 @@ import UIKit
 
 class AddDateReceiptViewController: UIViewController {
     
+    let receiptController = ReceiptController()
+    
     @IBOutlet var datePickerView: UIDatePicker!
+    
+    var locationString: String = ""
+    var dateString: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
     @IBAction func nextButtonPressed(_ sender: Any) {
-        
+        #warning("Need to implement to protect seguing without data")
+    }
+    
+    @IBAction func datePickerChanged(_ sender: Any) {
+        dateString = receiptController.dateFormatterConfig(datePickerView.date)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toPictureVC", let destination = segue.destination as? AddPictureReceiptViewController {
+            
+            destination.locationString = locationString
+            destination.dateString = dateString
+        }
     }
     
 }
--- ProjectManager/View Controllers/ReceiptsVC/ReviewReceiptViewController.swift
+++ ProjectManager/View Controllers/ReceiptsVC/ReviewReceiptViewController.swift
@@ -17,9 +17,29 @@ class ReviewReceiptViewController: UIViewController {
     @IBOutlet var priceLabel: UILabel!
     @IBOutlet var categoryLabel: UILabel!
     @IBOutlet var quantityLabel: UILabel!
+    @IBOutlet var descriptionTextView: UITextView!
+    
+    var locationString: String = ""
+    var dateString: String = ""
+    var nameString: String = ""
+    var priceString: String = ""
+    var quantityString: String = ""
+    var categoryString: String = ""
+    var descriptionString: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
+    }
+    
+    func updateViews() {
+        placeOfPurchaseLabel.text = locationString
+        dateLabel.text = dateString
+        itemNameLabel.text = nameString
+        priceLabel.text = priceString
+        quantityLabel.text = quantityString
+        categoryLabel.text = categoryString
+        descriptionTextView.text = descriptionString
 
     }
 
