f29c5732fb0ad48a32e41da15bc3b00d7a3e4e33
Jarren <jarrenmcampos@gmail.com>
2020-08-24 10:34:33 -0700

    Added photo librabry implementation

--- ProjectManager/View Controllers/ReceiptsVC/AddPictureReceiptViewController.swift
+++ ProjectManager/View Controllers/ReceiptsVC/AddPictureReceiptViewController.swift
@@ -15,12 +15,39 @@ class AddPictureReceiptViewController: UIViewController {
     var locationString: String = ""
     var dateString: String = ""
     
+    var originalImage: UIImage? {
+        didSet {
+            guard let originalImage = originalImage else {
+                return
+            }
+            receiptPreviewImage.image = originalImage
+            var scaledSize = receiptPreviewImage.bounds.size
+            let scale = UIScreen.main.scale
+            
+            scaledSize = CGSize(width: scaledSize.width*scale,
+                                height: scaledSize.height*scale)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    private func presentImagePickerController() {
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            print("The photo library is not available.")
+            return
+        }
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
 
     @IBAction func addImagePressed(_ sender: Any) {
-        
+        presentImagePickerController()
     }
     
     @IBAction func nextButtonPressed(_ sender: Any) {
@@ -33,4 +60,23 @@ class AddPictureReceiptViewController: UIViewController {
             destination.dateString = dateString
         }
     }
+}
+
+extension AddPictureReceiptViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[.editedImage] as? UIImage {
+            originalImage = image
+        } else if let image = info[.originalImage] as? UIImage {
+            originalImage = image
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
 }
