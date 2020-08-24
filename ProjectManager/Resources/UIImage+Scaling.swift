676de5378eb0331871e746d99c4781d33b9c2aeb
Jarren <jarrenmcampos@gmail.com>
2020-08-24 09:47:15 -0700

    Added UIImage extension file

---
+++ ProjectManager/Resources/UIImage+Scaling.swift
@@ -0,0 +1,38 @@
+//
+//  UIImage+Scaling.swift
+//  ProjectManager
+//
+//  Created by Jarren Campos on 8/24/20.
+//  Copyright © 2020 LambdaStudents. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    /// Resize the image to a max dimension from size parameter
+    func imageByScaling(toSize size: CGSize) -> UIImage? {
+        guard size.width > 0 && size.height > 0 else { return nil }
+        
+        let originalAspectRatio = self.size.width/self.size.height
+        var correctedSize = size
+        
+        if correctedSize.width > correctedSize.width*originalAspectRatio {
+            correctedSize.width = correctedSize.width*originalAspectRatio
+        } else {
+            correctedSize.height = correctedSize.height/originalAspectRatio
+        }
+        
+        return UIGraphicsImageRenderer(size: correctedSize, format: imageRendererFormat).image { context in
+            draw(in: CGRect(origin: .zero, size: correctedSize))
+        }
+    }
+    
+    /// Renders the image if the pixel data was rotated due to orientation of camera
+    var flattened: UIImage {
+        if imageOrientation == .up { return self }
+        return UIGraphicsImageRenderer(size: size, format: imageRendererFormat).image { context in
+            draw(at: .zero)
+        }
+    }
+}
