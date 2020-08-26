ff448445990d792851f1ae046a8cd1bc190c7095
Jarren <jarrenmcampos@gmail.com>
2020-08-25 20:07:37 -0700

    Basic implementation

--- ProjectManager/View Controllers/ProfileVC/ProfileViewController.swift
+++ ProjectManager/View Controllers/ProfileVC/ProfileViewController.swift
@@ -10,8 +10,39 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet var firtNameLabel: UILabel!
+    @IBOutlet var tableView: UITableView!
+    
+    var testArray: [String] = [
+    "Personal Information",
+    "Notifications",
+    "Get help",
+    "Give us feedback",
+    "Terms of Service",
+    "Log out"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
+    @IBAction func editProfileTapped(_ sender: Any) {
+        
+    }
+    
+}
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return testArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "profileCell", for: indexPath)
+        cell.textLabel?.text = testArray[indexPath.row]
+        cell.detailTextLabel?.text = "Testing"
+        
+        return cell
+    }
+    
+    
 }
