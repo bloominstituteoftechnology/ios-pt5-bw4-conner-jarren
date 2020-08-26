ed4b2b44e4eebda65becec58625ab4c0e417207d
Jarren <jarrenmcampos@gmail.com>
2020-08-25 19:41:00 -0700

    Added profileVC

---
+++ ProjectManager/View Controllers/ProfileVC/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  ProjectManager
+//
+//  Created by Jarren Campos on 8/25/20.
+//  Copyright © 2020 LambdaStudents. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
