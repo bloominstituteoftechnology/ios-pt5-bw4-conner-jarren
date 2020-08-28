6196a577dc0b5c9f6a38210ca47980bf2c9a26ce
Jarren <jarrenmcampos@gmail.com>
2020-08-27 20:22:16 -0700

    Changed array for now to hold a static array to work throughout app

---
+++ ProjectManager/View Controllers/DashboardVC/DashboardMainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DashboardMainViewController.swift
+//  ProjectManager
+//
+//  Created by Jarren Campos on 8/27/20.
+//  Copyright © 2020 LambdaStudents. All rights reserved.
+//
+
+import UIKit
+
+class DashboardMainViewController: UIViewController {
+    
+    @IBOutlet var totalAmountSpent: UILabel!
+    @IBOutlet var percentageToBudgetLabel: UILabel!
+    @IBOutlet var numberOfSavedProductsLabel: UILabel!
+    @IBOutlet var notesTotalLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("We are in the dashboard!!!\(ReceiptsTableViewController.receiptController.receipts)")
+    }
+    
+}
