86bed9a79859f2fb26565add75c4a4866686dd43
Jarren <jarrenmcampos@gmail.com>
2020-08-27 19:06:30 -0700

    Added mainVC and wired to SB

---
+++ ProjectManager/View Controllers/ProfileVC/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  ProjectManager
+//
+//  Created by Jarren Campos on 8/27/20.
+//  Copyright © 2020 LambdaStudents. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
