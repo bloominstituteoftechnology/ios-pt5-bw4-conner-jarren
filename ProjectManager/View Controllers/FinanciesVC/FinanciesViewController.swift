50f6be390dcf1d3e4e399779b6dbfc72b3096ca8
hai666l <0xconr@protonmail.com>
2020-08-29 16:55:30 -0500

    Finances VC implemented

--- ProjectManager/View Controllers/FinanciesVC/FinanciesViewController.swift
+++ ProjectManager/View Controllers/FinanciesVC/FinanciesViewController.swift
@@ -9,13 +9,71 @@
 import UIKit
 
 class FinanciesViewController: UIViewController {
+    @IBOutlet var pieChart: FinancialPieChart!
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var totalLabel: UILabel!
     
     var financialData: [String:Float] = [:]
+    var sortedKeys: [String] = []
+    let colors: [UIColor] = [.systemRed, .systemOrange, .systemYellow, .systemGreen, .systemBlue, .systemIndigo, .systemPurple]
+    var totalSpending: Float {
+        var total: Float = 0
+        for amount in financialData.values {
+            total += amount
+        }
+        return total
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.separatorStyle = .none
         financialData["Materials"] = 250.00
         financialData["Services"] = 150.00
-        print(financialData)
+        financialData["Misc"] = 25.00
+        financialData["Housing"] = 450.00
+        financialData["Gifts"] = 60.00
+        financialData["Food"] = 200.00
+        financialData["Some Other Category"] = 25.00
+        pieChart.drawPieChart(with: financialData)
+        sortedKeys = pieChart.sortData(financialData)
+        
+        totalLabel.text = String(format: "$%.2f", totalSpending)
+    }
+}
+
+extension FinanciesViewController: UITableViewDelegate, UITableViewDataSource {
+    // MARK: - TableView Stuff
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return financialData.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    // Set the spacing between sections
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 5
+    }
+    
+    // Make the background color show through spacing
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.clear
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as! FinancialTableViewCell
+        let category: String = sortedKeys[indexPath.section]
+        cell.configureViews(category: category, spending: financialData[category]!, color: colors[indexPath.section % 7])
+        return cell
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "CategorySegue" {
+            
+        }
     }
 }
--- ProjectManager/Views/Dashboard/Finances/FinancialPieChart.swift
+++ ProjectManager/Views/Dashboard/Finances/FinancialPieChart.swift
@@ -10,16 +10,82 @@ import UIKit
 
 class FinancialPieChart: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    let rainbow: [UIColor] = [.systemRed,
+                             .systemOrange,
+                             .systemYellow,
+                             .systemGreen,
+                             .systemBlue,
+                             .systemIndigo,
+                             .systemPurple]
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        self.backgroundColor = .lightGray
+        self.backgroundColor = .clear
+    }
+    
+    func drawPieChart(with data: [String:Float]) {
+        let center = CGPoint(x: self.bounds.maxX/2, y: self.bounds.maxY/2)
+        let circularPath = UIBezierPath(arcCenter: center, radius: 75, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        
+        // Calculate total of all spending in dictionary
+        var total: Float = 0
+        for (_, spending) in data {
+            total += spending
+        }
+        
+        // Sort keys of dictionary in ascending order
+        var sortedKeys: [String] = sortData(data)
+        
+        var rainbowIndex = 0
+        var start: CGFloat = 0
+        var occupiedSpace: CGFloat = 0
+        for key in sortedKeys {
+            let spending: Float = data[key]!
+            let end = CGFloat(spending/total) + occupiedSpace
+            occupiedSpace = end
+
+            let layer = CAShapeLayer()
+            layer.path = circularPath.cgPath
+            layer.fillColor = UIColor.clear.cgColor
+            layer.strokeColor = rainbow[rainbowIndex].cgColor
+            layer.lineWidth = 30
+            
+            layer.strokeStart = start
+            layer.strokeEnd = end
+            
+            self.layer.addSublayer(layer)
+            rainbowIndex += 1
+            if(rainbowIndex >= rainbow.count) { rainbowIndex = 0 }
+            start = end
+        }
+    }
+    
+    // Sort keys of data
+    func sortData(_ data: [String:Float]) -> [String] {
+        var sortedKeys: [String] = []
+        var keys = Set(data.keys)
+        
+        var smallestKey: String? = nil
+        var lastKey: String? = nil
+        
+        for _ in keys {
+            for key in keys {
+                if smallestKey != nil, lastKey != nil {
+                    if data[lastKey!]! > data[key]! {
+                        smallestKey = key
+                        lastKey = key
+                    }
+                } else {
+                    smallestKey = key
+                    lastKey = key
+                }
+            }
+            sortedKeys.append(smallestKey!)
+            keys.remove(smallestKey!)
+            smallestKey = nil
+            lastKey = nil
+        }
+        return sortedKeys
     }
 }
+
