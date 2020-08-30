a1d435121a10ea94860532c1d9b5765915487ba5
Jarren <jarrenmcampos@gmail.com>
2020-08-29 20:45:34 -0700

    Added graph functionality

--- ProjectManager/View Controllers/DashboardVC/BudgetViewController.swift
+++ ProjectManager/View Controllers/DashboardVC/BudgetViewController.swift
@@ -9,11 +9,48 @@
 import UIKit
 
 class BudgetViewController: UIViewController {
+    
+    var receiptController = ReceiptController()
 
+    @IBOutlet var sliderOutlet: UISlider!
+    @IBOutlet var budgetTotal: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        budgetTotal.text = receiptController.floatToStringConversion(sliderOutlet.value)
+        createGraph(UIColor.white.cgColor, 2 * CGFloat.pi, 1.0)
+        createGraph(UIColor.systemBlue.cgColor, 1 * CGFloat.pi, 1.0)
 
-        // Do any additional setup after loading the view.
     }
-
+    func createGraph(_ color: CGColor, _ endAngle: CGFloat, _ animated: CFTimeInterval) {
+        let shapeLayer = CAShapeLayer()
+        
+        let center = CGPoint(x: 210, y: 450)
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: 0, endAngle: endAngle, clockwise: false)
+        shapeLayer.path = circularPath.cgPath
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        
+        shapeLayer.strokeEnd = 0
+        shapeLayer.strokeColor = color
+        shapeLayer.lineWidth = 20
+        shapeLayer.lineCap =  CAShapeLayerLineCap.round
+        shapeLayer.lineJoin = CAShapeLayerLineJoin.round
+        
+        view.layer.addSublayer(shapeLayer)
+        
+        let animcolor = CABasicAnimation(keyPath: "strokeEnd")
+        animcolor.fromValue = UIColor.green.cgColor
+        animcolor.toValue = UIColor.orange.cgColor
+        animcolor.duration = animated
+        animcolor.isRemovedOnCompletion = false
+        animcolor.fillMode = CAMediaTimingFillMode.forwards
+        
+        shapeLayer.add(animcolor, forKey: "strokeEnd")
+        
+    }
+    
+    @IBAction func sliderChanged(_ sender: Any) {
+        budgetTotal.text = receiptController.floatToStringConversion(sliderOutlet.value)
+    }
+    
 }
