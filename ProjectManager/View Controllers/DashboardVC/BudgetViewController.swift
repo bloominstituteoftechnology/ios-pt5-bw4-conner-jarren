7fb56bcce7e416f716b5d184b4e7e601685fce8e
Jarren <jarrenmcampos@gmail.com>
2020-08-29 20:25:18 -0700

    Added budget vc.swift

---
+++ ProjectManager/View Controllers/DashboardVC/BudgetViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BudgetViewController.swift
+//  ProjectManager
+//
+//  Created by Jarren Campos on 8/29/20.
+//  Copyright © 2020 LambdaStudents. All rights reserved.
+//
+
+import UIKit
+
+class BudgetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
