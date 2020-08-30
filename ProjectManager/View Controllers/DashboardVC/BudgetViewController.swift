5d08c1eaea24af7a33392311f9866a281df36ed5
Jarren <jarrenmcampos@gmail.com>
2020-08-29 21:23:23 -0700

    Added linewidth to create graph func

--- ProjectManager/View Controllers/DashboardVC/BudgetViewController.swift
+++ ProjectManager/View Controllers/DashboardVC/BudgetViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class BudgetViewController: UIViewController {
     
     var receiptController = ReceiptController()
-    
+
     @IBOutlet var sliderOutlet: UISlider!
     @IBOutlet var budgetTotal: UILabel!
     @IBOutlet var widgetView: UIView!
@@ -19,9 +19,9 @@ class BudgetViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         budgetTotal.text = receiptController.floatToStringConversion(sliderOutlet.value)
-        createGraph(UIColor.white.cgColor, 2 * CGFloat.pi, 1.0)
-        createGraph(UIColor.systemBlue.cgColor, 1 * CGFloat.pi, 1.0)
-        
+        createGraph(UIColor.white.cgColor, 2 * CGFloat.pi, 1.0, 20)
+        createGraph(UIColor.systemBlue.cgColor, 1 * CGFloat.pi, 1.0, 20)
+
     }
     func run(after seconds: Int, completion: @escaping () -> Void) {
         let deadline = DispatchTime.now() + .seconds(seconds)
@@ -32,13 +32,13 @@ class BudgetViewController: UIViewController {
     
     func createGraphReset() {
         run(after: 1) {
-            self.createGraph(UIColor.white.cgColor, 2 * CGFloat.pi, 1.0)
-            self.createGraph(UIColor.systemBlue.cgColor, 1 * CGFloat.pi, 1.0)
+            self.createGraph(UIColor.white.cgColor, 2 * CGFloat.pi, 1.0, 20)
+            self.createGraph(UIColor.systemBlue.cgColor, 1 * CGFloat.pi, 1.0, 20)
         }
-        createGraph(widgetView.backgroundColor!.cgColor, 2 * CGFloat.pi, 1.0)
+        createGraph(widgetView.backgroundColor!.cgColor, 2 * CGFloat.pi, 1.0, 50)
     }
     
-    func createGraph(_ color: CGColor, _ endAngle: CGFloat, _ animated: CFTimeInterval) {
+    func createGraph(_ color: CGColor, _ endAngle: CGFloat, _ animated: CFTimeInterval, _ lineWidth: CGFloat) {
         let shapeLayer = CAShapeLayer()
         
         let center = CGPoint(x: 210, y: 450)
@@ -48,7 +48,7 @@ class BudgetViewController: UIViewController {
         
         shapeLayer.strokeEnd = 0
         shapeLayer.strokeColor = color
-        shapeLayer.lineWidth = 20
+        shapeLayer.lineWidth = lineWidth
         shapeLayer.lineCap =  CAShapeLayerLineCap.round
         shapeLayer.lineJoin = CAShapeLayerLineJoin.round
         
@@ -69,6 +69,7 @@ class BudgetViewController: UIViewController {
         budgetTotal.text = receiptController.floatToStringConversion(sliderOutlet.value)
         run(after: 1) {
             self.createGraphReset()
+
         }
     }
     
